47498b63105703afd14c24507145b3dee49804bf
anandgopalyadav <anandgopal2001@gmail.com>
2023-09-06 16:34:53 +0530

    update version

--- aj.py
+++ aj.py
@@ -1,16 +1,15 @@
 import pyttsx3
 print("Welcome to RoboSpeaker Programm....")
 engine = pyttsx3.init()
-engine.say("Ram Ram bhai Saryaa Nu")
+engine.say("Ram Ram ji Saareyaa Nu")
 engine.runAndWait()
 engine.say("Let's start")
 engine.runAndWait()
 while True:
     a = input("What you want me to Speak: ")
-    if a == "q":
-        engine.say("Bye Bye")
+    if a == "q" or "Q":
+        engine.say('Bye bye')
         engine.runAndWait()
         break
     engine.say(a)
     engine.runAndWait()
-    
